227f1a02aa46601117067a0a15b403951ffc7096
Stoneburner Software <sean@stoneburnersoftware.com>
2019-09-03 12:21:21 -0400

    created custom button, switch product image to local file

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import CustomButton from "../custom-button/custom-button.component";
+
 import "./product-card.styles.scss";
 
-const ProductCard = imgUrl => (
+const ProductCard = ({ children }) => (
   <div className="product-card">
     <div
       className="image"
       style={{
-        backgroundImage: `url(${"https://images-na.ssl-images-amazon.com/images/I/51ZCXF4QWTL._SX284_BO1,204,203,200_.jpg"})`
+        backgroundImage: "url(/book-images/IDC.jpg)"
       }}
     />
+    <CustomButton> Add to Cart </CustomButton>
+    <CustomButton> Share </CustomButton>
   </div>
 );
 
