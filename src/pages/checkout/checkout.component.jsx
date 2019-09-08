13ddd616775d30b628b9a744464309fa09580b38
Stoneburner Software <sean@stoneburnersoftware.com>
2019-09-08 16:50:04 -0400

    clear-item-from-cart action generator, to remove cart-item from checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import "./checkout.styles.scss";
+
 import { createStructuredSelector } from "reselect";
 import {
   selectCartItems,
